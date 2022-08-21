8328c021ca50abf47136743aef1d2ad96b4d7d55
nvmnghia <nvmnghia@gmail.com>
2022-08-21 15:18:46 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { atom, atomFamily, useRecoilCallback, useRecoilValue } from "recoil";
+
+export const Todos = atomFamily({
+  key: "Todos",
+  default: undefined,
+});
+
+export const TodoIds = atom({
+  key: "TodoIds",
+  default: [],
+});
+
+const Button = () => {
+  const items = [
+    { id: 1, content: "foo" },
+    { id: 2, content: "bar" },
+  ];
+
+  const onClick = useRecoilCallback(({ set }) => () => {
+    items.forEach((item) => set(Todos(item.id), item));
+    set(
+      TodoIds,
+      items.map((item) => item.id)
+    );
+  });
+
+  return <button onClick={onClick}>Load</button>;
+};
+
+const Todo = ({ id }) => {
+  const item = useRecoilValue(Todos(id));
+  return (
+    <div>
+      <p>{item.id}</p>
+      <p>{item.content}</p>
+    </div>
+  );
+};
+
+const TodoList = () => {
+  // ở đây m biết sẵn danh sách ID rồi đúng k? Lấy ra từ query client...
+  const ids = useRecoilValue(TodoIds);
+  console.log({ ids });
+
+  return (
+    <div>
+      {ids.map((id) => (
+        <Todo id={id} key={id} />
+      ))}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button />
+      <TodoList />
     </div>
   );
 }
